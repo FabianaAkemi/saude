0571e3370d7c8db491c207912432ada03bda5e19
fabiana <fabiana.fujimoto>
2017-10-26 12:51:43 +0200

    teste

---
+++ src/main/java/com/tcc/saude/security/GeradorSenha.java
@@ -0,0 +1,13 @@
+package com.tcc.saude.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GeradorSenha {
+
+  public static void main(String[] args) {
+	BCryptPasswordEncoder gerador = new  BCryptPasswordEncoder();
+
+	System.out.println(gerador.encode("teste"));			
+			
+}
+}
